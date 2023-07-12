54531b599f44fe68bdbc803057c7aef1f7143e0c
michael.nguyen <mnguyen@deloitte.co.uk>
2023-07-12 12:38:21 +0100

    feat: Add prop types

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
-import './App.css';
-import pokemon from './pokemon.json';
+import React from "react";
+import pokemon from "./pokemon.json";
+import PropTypes from "prop-types";
+
+import "./App.css";
+
+const PokemonRow = ({ pokemon }) => (
+    <tr>
+      <td>{pokemon.name.english}</td>
+      <td>{pokemon.type.join(", ")}</td>
+    </tr>
+);
+
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.shape({
+      english: PropTypes.string.isRequired,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string.isRequired),
+  })
+};
 
 function App() {
   return (
-    <div style={{
-      margin: "auto", 
-      width: 800,
-      paddingTop: "1rem",
-    }}
+    <div
+      style={{
+        margin: "auto",
+        width: 800,
+        paddingTop: "1em",
+      }}
     >
-      <h1 className='title'>Pokemon Search</h1>
+      <h1 className="title">Pokemon Search</h1>
       <table width="100%">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-       <tbody>
-        {pokemon.slice(0, 20).map((pokemon) => ( 
-          <tr key={pokemon.id}>
-            <td>{pokemon.name.english}</td>
-            <td>{pokemon.type.join(', ')}</td>
-          </tr>
-        ))}
-       </tbody>
+        <tbody>
+          {pokemon.slice(0, 20).map((pokemon) => (
+            <PokemonRow key={pokemon.id} pokemon={pokemon} />
+          ))}
+        </tbody>
       </table>
     </div>
   );
