d9d95e180f7f340aa8d883b5c9c956a067e7fb88
michael.nguyen <mnguyen@deloitte.co.uk>
2023-07-18 14:29:08 +0100

    css: Add material ui dependency

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
-
+import {Button} from '@mui/material';
 
 const PokemonRow = ({ pokemon, onSelect }) => (
     <tr>
       <td>{pokemon.name.english}</td>
       <td>{pokemon.type.join(", ")}</td>
       <td>
-        <button onClick={() => onSelect(pokemon)}>Select!</button>
+        <Button variant="contained" onClick={() => onSelect(pokemon)}>Select!</Button>
       </td>
     </tr>
 );
