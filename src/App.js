2d89d71ffa97da66c58a07b4434e916823abc8f3
michael.nguyen <mnguyen@deloitte.co.uk>
2023-07-18 14:17:47 +0100

    css: use styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-
 import PropTypes from "prop-types";
-import "./App.css";
+import styled from "@emotion/styled";
+
 
 const PokemonRow = ({ pokemon, onSelect }) => (
     <tr>
@@ -57,6 +57,31 @@ PokemonInfo.propTypes = {
   })
 };
 
+const Title = styled.h1`
+text-align: center
+`;
+
+const TwoColumnLayout = styled.div`
+  display: grid;
+  grid-template-columns: 70%, 30%;
+  grid-column-gap: 1rem;
+`;
+
+const Container = styled.div`
+  margin: auto,
+  width: 800px,
+  paddingTop: 1rem,
+`
+const Input = styled.input`
+  width: 100%;
+  padding: 0.2rem;
+  font-size: x-large;
+`
+const TableHeader = styled.th`
+  text-align: left;
+  font-size: x-large;
+`
+
 function App() {
   // React hook
   const [filter, filterSet] = React.useState(""); // return current state (filter) and filterSet sets value of the filter (event)
@@ -71,32 +96,20 @@ function App() {
   }, []);
 
   return (
-    <div
-      style={{
-        margin: "auto",
-        width: 800,
-        paddingTop: "1rem",
-      }}
-    >
-      <h1 className="title">Pokemon Search</h1>
+    <Container>
+      <Title>Pokemon Search</Title>
   
-      <div 
-        style={{
-        display: "grid",
-        gridTemplateColumns: "70%, 30%",
-        gridColumnGap: "1rem",
-      }}
-      >
+      <TwoColumnLayout>
         <div>
-          <input 
+          <Input
             value={filter} 
             onChange={(event) => filterSet(event.target.value)}
           />
           <table width="100%">
             <thead>
                 <tr>
-                  <th>Name</th>
-                  <th>Type</th>
+                  <TableHeader>Name</TableHeader>
+                  <TableHeader>Type</TableHeader>
                 </tr>
             </thead>
             <tbody>
@@ -110,8 +123,8 @@ function App() {
           </table>
         </div>
         {selectedItem && (<PokemonInfo {...selectedItem} />)}
-      </div>
-    </div>
+      </TwoColumnLayout>
+    </Container>
   );
 }
 
